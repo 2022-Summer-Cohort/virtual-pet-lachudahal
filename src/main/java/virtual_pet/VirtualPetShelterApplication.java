3b130d2c495472bcea320dfdc880c86d3e5b6d65
Lachu Dahal <dahallachu96@gmail.com>
2022-06-07 21:41:25 -0400

    VirtualPetShelter without Stretch Tasks

--- src/main/java/virtual_pet/VirtualPetShelterApplication.java
+++ src/main/java/virtual_pet/VirtualPetShelterApplication.java
@@ -3,7 +3,7 @@ package virtual_pet;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class VirtualPetApplication {
+public class VirtualPetShelterApplication {
 
     public static void feedAllPets(VirtualPetShelter petShelter) {
         petShelter.feedAll();
